65ba8ad5ec26ae3d729ac6ed2c5325247ace38d9
B Severino <biancasev@gmail.com>
2020-03-27 21:41:47 -0400

    Form validation completed

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -5,18 +5,46 @@ import {
   Submit,
   FieldError,
   Label,
+  FormError
 } from '@redwoodjs/web'
+import { useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
 import BlogLayout from 'src/layouts/BlogLayout'
 
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: ContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
 const ContactPage = () => {
-  const onSubmit = data => console.log(data)
+  const formMethods = useForm({ mode: 'onBlur' })
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      alert('Thank you for your submission!')
+      formMethods.reset()
+    }
+  })
+
+  const onSubmit = data => {
+    create({ variables: { input: data }})
+    console.log(data)
+  }
 
   return (
     <BlogLayout>
       <Form
         onSubmit={onSubmit}
         validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
       >
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+        />
         <Label
           name='name'
           style={{ display: 'block' }}
@@ -45,10 +73,6 @@ const ContactPage = () => {
           errorStyle={{ display: 'block', borderColor: 'red' }}
           validation={{
             required: true,
-            pattern: {
-              value: /[^@]+@[^.]+\..+/,
-              message: 'Please enter a valid email address',
-            }
           }}
         />
         <FieldError name='email' style={{ color: 'red' }} />
@@ -70,7 +94,10 @@ const ContactPage = () => {
         />
         <FieldError name='message' style={{ color: 'red' }} />
 
-        <Submit style={{ display: 'block' }}>Save</Submit>
+        <Submit
+          style={{ display: 'block' }}
+          disabled={loading}
+        >Save</Submit>
       </Form>
     </BlogLayout>
   )
